d0e58f5bb8f5523e7ae3f087d8d3881d7e81b23a
JIYOUNG PARK <ziy913@naver.com>
2022-09-19 23:07:22 +0900

    [Week3] Distinct Subsequences

---
+++ DistinctSub.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+// Check string matches from the back
+// Repeat as a recursive function and add 1 if there is a string
+int subseq(char* strX, char* strZ, int m, int n) {
+
+	if (n > m) {
+		return 0;
+	}
+
+	if (m == 0 || n == 0) {
+		return 1;
+	}
+	else if (strX[m - 1] == strZ[n - 1]) {
+		return subseq(strX, strZ, m - 1, n) + subseq(strX, strZ, m - 1, n - 1);
+	}
+	else
+		return subseq(strX, strZ, m - 1, n);
+}
+
+void main() {
+	
+	char stringX[10000];
+	char stringZ[100];
+	int num = 0;
+
+	printf("Enter the number of test cases : ");
+	scanf("%d", &num);
+
+	for (int i = 0; i < num; i++) {
+
+		printf("Enter string X : ");
+		scanf("%s", stringX);
+
+		printf("Enter string Z : ");
+		scanf("%s", stringZ);
+
+		printf("The number of distinct occurrences of Z in X : %d\n", subseq(stringX, stringZ, strlen(stringX), strlen(stringZ)));
+	}
+
+}
